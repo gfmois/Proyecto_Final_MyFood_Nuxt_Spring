e9c4b39ccb74ceab0cf74472bea3d0127ccdebeb
Moises Guerola <daw.moisesguerola@gmail.com>
2023-03-11 11:34:10 +0100

    Final Project Started

---
+++ backend/client/src/main/java/com/pf_nsl_myfood/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.pf_nsl_myfood.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
