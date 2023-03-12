b4330dad76ee2995033cde5e6503ee87159d45f3
Moises Guerola <daw.moisesguerola@gmail.com>
2023-03-12 12:54:01 +0100

    (Frontend) - Home Page Started, Button, Navbar and Footer Components Created

---
+++ frontend/components/layout/Button.vue
@@ -0,0 +1,24 @@
+<script setup>
+const { title, buttonType, customStyle } = defineProps({
+    title: String,
+    buttonType: String,
+    customStyle: String
+})
+
+const styles = {
+    default: "text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800",
+    red: "text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900",
+    green: "text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800",
+}
+
+if (customStyle && buttonType == 'custom') {
+    styles["custom"] = `${styles["default"]} ${customStyle}`
+}
+</script>
+
+<template>
+    <div>
+        <button type="button"
+            :class="`${styles[buttonType] || styles['default']} !p-4`">{{ title }}</button>
+    </div>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,39 +1,13 @@
-<script>
-import "vue3-carousel/dist/carousel.css"
-import { Carousel, Slide, Navigation } from "vue3-carousel"
-
-export default {
-    components: {
-        Carousel,
-        Slide,
-        Navigation
-    },
-    setup() {
-        return { images: ["restaurant_1.png", "restaurant_2.png", "restaurant_3.png", "restaurant_4.png", "restaurant_5.png", "restaurant_6.png",] }
-    }
-}
-</script>
-
 <template>
-    <div class="h-[75vh] w-full p-[10px] box-border grid grid-cols-1 grid-rows-1 gap-y-[10px] gap-x-[10px] tracking-[2px] text-[1.5rem]">
-        <div class="w-full h-full bg-red-400 overflow-hidden slide-item">
-            <carousel :autoplay="3000" :wrap-around="true" :transition="1500">
-                <slide v-for="image in images" :key="image">
-                    <img :src="`/images/${image}`" alt="Restaurant Image" class="w-full h-full object-none object-center md:object-bottom">
-                </slide>
-            </carousel>
+    <div
+        class="h-[75vh] w-full p-[10px] box-border grid grid-cols-1 grid-rows-1 gap-y-[10px] gap-x-[10px] tracking-[2px] text-[1.5rem] relative">
+        <div class="w-full h-full bg-red-400 overflow-hidden filter brightness-50 relative">
+            <img src="/images/home.jpg" alt="MyFood Welcome Image" class="h-full w-full object-cover object-center">
+        </div>
+        <div class="absolute w-1/3 h-1/3 z-50 top-20 left-20 bg-white/80 p-6 text-black rounded-lg shadow-lg font-mono text-center">
+            <h1 class="">Bienvenido a MyFood</h1>
+            <p class="text-sm">La Aplicación que te servirá para llenar tu apetito</p>
+            <LayoutButton title="Comienza tu Experiencia" class="text-center mt-20" />
         </div>
     </div>
 </template>
-
-<style>
-.slide-item {
-    grid-area: 1 / 1 / 3 / 4;
-    background-position: center bottom;
-}
-
-img {
-    object-fit: none;
-    object-position: center bottom;
-  }
-</style>
